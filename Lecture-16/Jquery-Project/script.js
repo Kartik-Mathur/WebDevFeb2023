1924c0964a05ae035105ebbf2371bc2696211056
Kartik <kartik.mathur10@gmail.com>
2023-04-08 14:14:48 +0530

    jQuery only TodoList, Lecture-16

--- Lecture-16/Jquery-Project/script.js
+++ Lecture-16/Jquery-Project/script.js
@@ -1,2 +1,76 @@
 $('body')
-    .append()
+    .append(
+        $('<div>')
+            .addClass('container')
+            .append(
+                $('<h1>')
+                    .text('Todo List')
+            )
+            .append(
+                $('<input>')
+                    .addClass('inp')
+            )
+            .append(
+                $('<button>')
+                    .addClass('btn')
+                    .text('Click Me')
+                    .click(() => {
+                        $('.taskList')
+                            .append(
+                                $('<li>')
+                                    .addClass('item')
+                                    .append(
+                                        $('<div>')
+                                            .addClass('taskInput')
+                                                .append(
+                                                    $('<input>')
+                                                        .attr('type','checkbox')
+                                                        .addClass('checkbox')
+                                                )
+                                                .append(
+                                                    $('<span>')
+                                                        .addClass('task')
+                                                        .text($('.inp').val())
+                                                )
+                                    )
+                                    .append(
+                                        $('<div>')
+                                            .addClass('buttons')
+                                                .append(
+                                                    $('<span>')
+                                                        .addClass('upArrow')
+                                                        .text('↑')
+                                                        .click(function(){
+                                                            let Grandparent = $(this).parent().parent()
+                                                            console.log(Grandparent)
+                                                            $(Grandparent).insertBefore(Grandparent.prev())
+                                                        })
+                                                )
+                                                .append(
+                                                    $('<span>')
+                                                        .addClass('downArrow')
+                                                        .text('↓')
+                                                        .click(function(){
+                                                            let Grandparent = $(this).parent().parent()
+                                                            console.log(Grandparent)
+                                                            $(Grandparent).insertAfter(Grandparent.next())
+                                                        })
+                                                )
+                                                .append(
+                                                    $('<span>')
+                                                        .addClass('deleteTask')
+                                                        .text('🗑')
+                                                        .click(function(){
+                                                            $(this).parent().parent().remove()
+                                                        })
+                                                )
+                                    )
+                                    
+                            )
+                    })
+            )
+            .append(
+                $('<div>')
+                    .addClass('taskList')
+            )
+    )
