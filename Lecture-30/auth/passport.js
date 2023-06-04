7c335271e6d5096e925dd94328df1854fbbc4fb2
Kartik <kartik.mathur10@gmail.com>
2023-06-04 15:52:49 +0530

    Passport authentication

--- Lecture-30/auth/passport.js
+++ Lecture-30/auth/passport.js
@@ -1,18 +1,22 @@
 const passport = require('passport');
 const User = require('../models/users');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    },
+    usernameField: 'email',
+    passwordField: 'password'
+},
     async function (email, password, done) {
         console.log("Here", email, password);
         try {
             let user = await User.findOne({ username: email });
             if (!user) { return done(null, false); }
-            if (user.password != password) return done(null, false);
-            return done(null, user);
+            bcrypt.compare(password, user.password).then(function (result) {
+                if (result==false) return done(null, false);
+                return done(null, user);
+            });
+
         }
         catch (err) {
             if (err) { return done(err); }
--- Lecture-30/routes/signup.js
+++ Lecture-30/routes/signup.js
@@ -2,37 +2,44 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.render('signup');
 });
 
-router.post('/',async (req,res)=>{
-    const {email,password} = req.body;
-    console.log(email,password);
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
     let username = email;
-    try{
-        let user = await User.findOne({username});
-        if(user){
-            res.render('login',{
-                msg:"User is Already Present"
+    try {
+        let user = await User.findOne({ username });
+        if (user) {
+            res.render('login', {
+                msg: "User is Already Present"
             });
         }
-        else{
-            await User.create({
-                username,
-                password
-            })
-            res.render('login',{
-                msg: "Signup Success"
+        else {
+            bcrypt.genSalt(saltRounds, async function (err, salt) {
+                bcrypt.hash(password, salt, async function (err, hash) {
+                    await User.create({
+                        username,
+                        password:hash
+                    })
+                    res.render('login', {
+                        msg: "Signup Success"
+                    });
+                });
             });
+
         }
     }
-    catch(err){
+    catch (err) {
         res.send(err);
     }
-        
+
 })
 
 
-module.exports=router;
+module.exports = router;
