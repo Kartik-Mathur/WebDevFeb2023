68f6a88198398f6032c3ed9d820efab067bfaa53
Kartik <kartik.mathur10@gmail.com>
2023-06-14 19:57:26 +0530

    React JS Basics

---
+++ Lecture-32/myapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import  './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className="div1">
+        I am the APP Component
+      </div>
+      <div className="div2">
+        Another div inside app component
+      </div>
+    </div>
+  )
+}
+
+
+export default App
